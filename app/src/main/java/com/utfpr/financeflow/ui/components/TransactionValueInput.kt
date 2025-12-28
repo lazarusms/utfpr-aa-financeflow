6def923f46648586c693ff4a24a3f2c48952593b
lazarusms <lazarusmathaus@gmail.com>
2025-12-28 19:51:39 -0300

    Alterado tela lancamento de transacao

---
+++ app/src/main/java/com/utfpr/financeflow/ui/components/TransactionValueInput.kt
@@ -0,0 +1,79 @@
+package com.utfpr.financeflow.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.utfpr.financeflow.ui.theme.FinanceFlowTheme
+
+@Composable
+fun TransactionValueInput(label: String, value: String, onValueChange: (String) -> Unit) {
+    Column(modifier = Modifier.padding(bottom = 16.dp)) {
+        Text(
+            text = "Adicionar nova transação:",
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        BasicTextField(
+            value = value,
+            onValueChange = { newValue ->
+                if (newValue.length <= 7) {//verificar de sempre ter o "."
+                    onValueChange(newValue)
+                }
+            },
+            textStyle = TextStyle(
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            ),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "R$",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 40.sp,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    innerTextField()
+                }
+            }
+        )
+        Text(
+            text = label,
+            modifier = Modifier.padding(top = 8.dp),
+            color = Color(0x947E817E),
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun TransactionValueInputPreview() {
+    FinanceFlowTheme {
+        TransactionValueInput(
+            label = "Valor",
+            value = "125.00",
+            onValueChange = {}
+        )
+    }
+}
--- app/src/main/java/com/utfpr/financeflow/ui/screens/AddTransactionScreen.kt
+++ app/src/main/java/com/utfpr/financeflow/ui/screens/AddTransactionScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,14 +20,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.utfpr.financeflow.model.TransactionViewModel
 import com.utfpr.financeflow.ui.components.TransactionDropdown
 import com.utfpr.financeflow.ui.components.TransactionInputField
+import com.utfpr.financeflow.ui.components.TransactionValueInput
 import com.utfpr.financeflow.ui.theme.FinanceFlowTheme
 
 @Composable
@@ -37,7 +35,7 @@ fun AddTransactionScreen(
 ) {
     var date by rememberSaveable { mutableStateOf("") }
     var category by rememberSaveable { mutableStateOf("") }
-    var amount by rememberSaveable { mutableStateOf("") }
+    var amount by rememberSaveable { mutableStateOf("0.00") }
     var description by rememberSaveable { mutableStateOf("") }
 
     Column(
@@ -47,19 +45,9 @@ fun AddTransactionScreen(
             .padding(24.dp)
             .verticalScroll(rememberScrollState())
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("Novo Lançamento", fontWeight = FontWeight.ExtraBold, fontSize = 18.sp)
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-
+        TransactionValueInput(label = "Valor", value = amount, onValueChange = { amount = it })
+        TransactionDropdown  (label = "Tipo", value = category, options = listOf("RECEITA", "DESPESA"), onValueChange = { category = it })//TODO passar pra ENUM ou banco
         TransactionInputField(label = "Data do Lançamento:", value = date, onValueChange = { date = it })
-       // ExpenseInputField(label = "Tipo:", value = category, onValueChange = { category = it })
-        //TODO - trocar pra radio por conta da doc.?
-        TransactionDropdown(label = "Tipo", value = category, options = listOf("RECEITA", "DESPESA"), onValueChange = { category = it })//TODO passar pra ENUM ou banco
-        TransactionInputField(label = "Valor:", value = amount, onValueChange = { amount = it })
         TransactionInputField(label = "Descrição:", value = description, onValueChange = { description = it })
 
         Spacer(modifier = Modifier.weight(1f))
@@ -76,7 +64,7 @@ fun AddTransactionScreen(
             onClear = {
                 date = ""
                 category = ""
-                amount = ""
+                amount = "0.00"
                 description = ""
             }//TODO voltar pro primeiro campo no clear e salvar estado qnd volta de extrato
         )
@@ -86,7 +74,7 @@ fun AddTransactionScreen(
 @Composable
 fun ExpenseActionButtons(onSave: () -> Unit = {}, onClear: () -> Unit = {}) {
     Row(
-        modifier = Modifier.fillMaxWidth(),// deixar exposto?
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Button(
