ede6d6d417e1925ae99a20eb0da0dbd40d2c50ad
lazarusms <lazarusmathaus@gmail.com>
2026-01-04 01:22:45 -0300

    ajuste na model de transaction

--- app/src/main/java/com/utfpr/financeflow/model/Transaction.kt
+++ app/src/main/java/com/utfpr/financeflow/model/Transaction.kt
@@ -1,10 +1,6 @@
 package com.utfpr.financeflow.model
 
 import java.time.LocalDate
-
-//classe temporaria pra mockar dados de transação
-// alterar para a correta
-// ter uma pasta model e repository ?
 data class Transaction(
     val description: String,
     val date: LocalDate,
